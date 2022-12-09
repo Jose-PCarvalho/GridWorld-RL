d80d7b352bb6566911a8c54e48113da0c7b0844a
José Pedro <up201806263@g.uporto.pt>
2022-12-09 01:13:56 +0000

    refactoring guru

--- environment.py
+++ environment.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 from enum import Enum
 import pygame
@@ -5,6 +7,7 @@ import gym
 from gym import spaces
 from gym.spaces import MultiDiscrete, Discrete
 from collections import defaultdict
+from collections import deque
 
 
 class State:
@@ -15,22 +18,25 @@ class State:
         self.state = obs
 
 
+class Queue:
+    def __init__(self):
+        self.elements = deque()
+
+    def empty(self) -> bool:
+        return not self.elements
+
+    def put(self, x):
+        self.elements.append(x)
+
+    def get(self):
+        return self.elements.popleft()
+
+
 class Actions(Enum):
     UP = 0
     Down = 2
     Left = 3
     Right = 1
-
-
-class GridTile:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.x_y(x, y)
-        self.visited = False
-
-    def visit(self):
-        self.visited = True
 
 
 class GridMap:
@@ -39,6 +45,7 @@ class GridMap:
             self.height = a.shape[0]
             self.width = a.shape[1]
             self.map = {(0, 0): []}
+            self.visited_list = [(0, 0)]
             for i in range(self.width):
                 for j in range(self.height):
                     self.new_tile((i, j))
@@ -46,6 +53,7 @@ class GridMap:
             self.height = 1
             self.width = 1
             self.map = {(0, 0): []}
+            self.visited_list = [(0, 0)]
 
     @staticmethod
     def adjacentTiles(tile):
@@ -75,6 +83,10 @@ class GridMap:
             print(t, ": ", self.map[t])
         print("end\n")
 
+    def visit_tile(self, tile):
+        if tile not in self.visited_list:
+            self.visited_list.append((tile[0], tile[1]))
+
     def graph_to_array(self):
         a = np.zeros((self.height, self.width), dtype=int)
 
@@ -83,9 +95,70 @@ class GridMap:
                 tile = (i, j)
                 if tile not in self.getTiles():
                     a[i, j] = -1
+                elif tile in self.visited_list:
+                    a[i, j] = 1
 
         return a
-    # def is_reachable(self,start,finish):
+
+    def coverage_array(self, full_map):
+        size = full_map.height
+        a = np.zeros((size, size), dtype=int)
+
+        for i in range(size):
+            for j in range(size):
+                tile = (i, j)
+                if tile in self.visited_list:
+                    a[i, j] = 1
+
+        return a
+
+    def breadth_first_search(self, start, finish):
+        # print out what we find
+        frontier = Queue()
+        frontier.put(start)
+        reached = {start: True}
+        reachable = False
+
+        while not frontier.empty():
+            current = frontier.get()
+            if current == finish:  # early exit
+                reachable = True
+                break
+
+            for next in self.map[current]:
+                if (next not in reached) and (next not in self.visited_list):
+                    frontier.put(next)
+                    reached[next] = True
+        return reachable
+
+    def laser_scanner(self, tile, full_map):
+        r = 3
+        tiles = {"up": [],
+                 "down": [],
+                 "right": [],
+                 "left": []}
+        for i in range(r):
+            tiles["up"].append((tile[0] - i, tile[1]))
+            tiles["down"].append((tile[0] + i, tile[1]))
+            tiles["right"].append((tile[0], tile[1] + i))
+            tiles["left"].append((tile[0], tile[1] - i))
+        directions = tiles.keys()
+        to_remove = []
+
+        for dir in directions:
+            to_remove.clear()
+            for t in tiles[dir]:
+                if t in full_map.getTiles():
+                    self.new_tile(t)
+                    # self.print_graph()
+                else:
+                    to_remove.append(t)
+
+                if t in self.visited_list:
+                    to_remove.append(t)
+            for rem in to_remove:
+                tiles[dir].remove(rem)
+        return tiles
 
 
 class GridWorld:
@@ -119,130 +192,37 @@ class GridWorld:
         self.agent_R = 0.0
 
         # Reset agent1's visited fields.
-        self.agent_visited_fields = set([tuple(self.agent_pos)])
-        self.agent_seen_fields = set([tuple(self.agent_pos)])
         self.map = np.zeros((self.height, self.width), dtype=int)
-        self.graph_map = GridMap(self.map)
+        self.full_graph = GridMap(self.map)
         self.map[self.agent_pos[0], self.agent_pos[1]] = 1
-        self.coverage_map = np.zeros((self.height, self.width), dtype=int)
-        self.coverage_map[self.agent_pos[0], self.agent_pos[1]] = 1
-        self.slam_map = np.zeros((self.height, self.width), dtype=int)
-        self.slam_map[self.agent_pos[0], self.agent_pos[1]] = 1
         self.graph = GridMap()
         self.remaining = self.height * self.width - 1;
         # How many timesteps have we done in this episode.
         self.timesteps = 0
         # Return the initial observation in the new episode.
+        self.graph.laser_scanner((self.agent_pos[0], self.agent_pos[1]), self.full_graph)
         return self._get_obs(), self._get_info()
 
     def _get_obs(self):
         # limit_up, limit_down, limit_right, limit_left = self.find_limit()
         # self._state.update(np.array([limit_up, limit_down, limit_right, limit_left]))
         # return limit_up, limit_down, limit_right, limit_left
-        # obs=tuple(map(tuple, self.coverage_map))
 
-        obs = tuple(map(tuple, self.coverage_map)) + tuple(map(int, self.agent_pos))
+        obs = tuple(map(tuple, self.graph.coverage_array(self.full_graph))) + tuple(map(int, self.agent_pos))
         return obs
 
     def _get_info(self):
-        limit_up, limit_down, limit_right, limit_left, new_seen = self.find_limit()
-        return limit_up, limit_down, limit_right, limit_left
-
-    def find_limit(self):
-        up = True
-        down = True
-        left = True
-        right = True
-        limit_up = 0
-        limit_down = 0
-        limit_right = 0
-        limit_left = 0
-        new_seen = 0
-        r = 3
-        for i in range(1, r):
-            if down:
-
-                if self.agent_pos[0] == self.height - 1:
-                    limit_down = 0
-                    down = False
-                elif not (self.agent_pos[0] + i, self.agent_pos[1]) in self.agent_seen_fields:
-                    self.agent_seen_fields.add((self.agent_pos[0] + i, self.agent_pos[1]))
-                    self.slam_map[self.agent_pos[0] + i, self.agent_pos[1]] = 1
-                    self.graph.new_tile((self.agent_pos[0] + i, self.agent_pos[1]))
-                    new_seen += 1
-                elif (self.agent_pos[0] + i == self.height - 1) and (
-                        self.coverage_map[self.agent_pos[0] + i, self.agent_pos[1]] == 0):
-                    limit_down = i
-                    down = False
-                elif self.coverage_map[self.agent_pos[0] + i, self.agent_pos[1]] != 0:
-                    limit_down = i - 1
-                    down = False
-
-            if up:
-
-                if self.agent_pos[0] == 0:
-                    limit_up = 0
-                    up = False
-                elif not (self.agent_pos[0] - i, self.agent_pos[1]) in self.agent_seen_fields:
-                    self.agent_seen_fields.add((self.agent_pos[0] - i, self.agent_pos[1]))
-                    self.slam_map[self.agent_pos[0] - i, self.agent_pos[1]] = 1
-                    self.graph.new_tile((self.agent_pos[0] - i, self.agent_pos[1]))
-                    new_seen += 1
-                elif (self.agent_pos[0] - i == 0) and (
-                        self.coverage_map[self.agent_pos[0] - i, self.agent_pos[1]] == 0):
-                    limit_up = i
-                    up = False
-                elif self.coverage_map[self.agent_pos[0] - i, self.agent_pos[1]] != 0:
-                    limit_up = i - 1
-                    up = False
-
-            for i in range(1, r):
-                if right:
-
-                    if self.agent_pos[1] == self.width - 1:
-                        limit_right = 0
-                        right = False
-                    elif not (self.agent_pos[0], self.agent_pos[1] + i) in self.agent_seen_fields:
-                        self.agent_seen_fields.add((self.agent_pos[0], self.agent_pos[1] + i))
-                        self.slam_map[self.agent_pos[0], self.agent_pos[1] + i] = 1
-                        self.graph.new_tile((self.agent_pos[0], self.agent_pos[1] + i))
-                        new_seen += 1
-                    elif (self.agent_pos[1] + i == self.width - 1) and (
-                            self.coverage_map[self.agent_pos[0], self.agent_pos[1] + i] == 0):
-                        limit_right = i
-                        right = False
-                    elif self.coverage_map[self.agent_pos[0], self.agent_pos[1] + i] != 0:
-                        limit_right = i - 1
-                        right = False
-
-                if left:
-
-                    if self.agent_pos[1] == 0:
-                        limit_left = 0
-                        left = False
-                    elif not (self.agent_pos[0], self.agent_pos[1] - i) in self.agent_seen_fields:
-                        self.agent_seen_fields.add((self.agent_pos[0], self.agent_pos[1] - i))
-                        self.slam_map[self.agent_pos[0], self.agent_pos[1] - i] = 1
-                        self.graph.new_tile((self.agent_pos[0], self.agent_pos[1] - i))
-                        new_seen += 1
-                    elif self.agent_pos[1] - i == 0 and (
-                            self.coverage_map[self.agent_pos[0], self.agent_pos[1] - i] == 0):
-                        limit_left = i
-                        left = False
-                    elif self.coverage_map[self.agent_pos[0], self.agent_pos[1] - i] != 0:
-                        limit_left = i - 1
-                        left = False
-
-        return limit_up, limit_down, limit_right, limit_left, new_seen
+        # limit_up, limit_down, limit_right, limit_left, new_seen = self.find_limit()
+        # return limit_up, limit_down, limit_right, limit_left
+        return None, None, None, None
 
     def step(self, action):
         # increase our time steps counter by 1.
         self.timesteps += 1
         events = self._move(self.agent_pos, action)
+        self.graph.laser_scanner((self.agent_pos[0], self.agent_pos[1]), self.full_graph)
         # Get observations (based on new agent positions).
         obs = self._get_obs()
-        self.closest()
-
         r = 0
         if "agent_new_field" in events:
             r = 1 / (self.width * self.height)
@@ -257,17 +237,7 @@ class GridWorld:
         info = self._get_info()
         if self.render_mode == "human":
             self.render()
-        self.graph.graph_to_array()
         return obs, r, terminated, info  # <- info dict (not needed here).
-
-    def closest(self):
-        non_explored = np.argwhere(self.coverage_map == 0)
-        if non_explored.size == 0:
-            return 0
-        distances = non_explored - self.agent_pos
-        distances = np.linalg.norm(distances, axis=1)
-        min_i = np.argmin(distances)
-        return non_explored[min_i]
 
     def _move(self, coords, action):
         # Change the row: 0=up (-1), 2=down (+1)
@@ -291,9 +261,8 @@ class GridWorld:
         if blocked:
             return {"Blocked"}
         # If agent1 -> "new" if new tile covered.
-        if not tuple(coords) in self.agent_visited_fields:
-            self.agent_visited_fields.add(tuple(coords))
-            self.coverage_map[self.agent_pos[0], self.agent_pos[1]] = 1
+        if not tuple(coords) in self.graph.visited_list:
+            self.graph.visit_tile((coords[0], coords[1]))
             self.remaining -= 1;
             return {"agent_new_field"}
         # No new tile for agent1.
@@ -307,20 +276,30 @@ class GridWorld:
             self.window = pygame.display.set_mode((self.window_size, self.window_size))
         if self.clock is None and self.render_mode == "human":
             self.clock = pygame.time.Clock()
-
+        size = max(self.graph.width, self.graph.height)
         canvas = pygame.Surface((self.window_size, self.window_size))
-        canvas.fill((255, 255, 255))
+        canvas.fill((0, 0, 0))
         pix_square_size = (
-                self.window_size / self.size
+                self.window_size / size
         )  # The size of a single grid square in pixels
 
         # First we draw the target
-        for i in range(self.size):
-            for j in range(self.size):
-                if self.slam_map[i, j] == 1:
+        a = self.graph.graph_to_array()
+        for i in range(self.graph.height):
+            for j in range(self.graph.width):
+                if self.graph.graph_to_array()[i, j] == 1:
                     pygame.draw.rect(
                         canvas,
                         (255, 0, 0),
+                        pygame.Rect(
+                            pix_square_size * np.array([j, i]),
+                            (pix_square_size, pix_square_size),
+                        ),
+                    )
+                elif self.graph.graph_to_array()[i, j] == 0:
+                    pygame.draw.rect(
+                        canvas,
+                        (255, 255, 255),
                         pygame.Rect(
                             pix_square_size * np.array([j, i]),
                             (pix_square_size, pix_square_size),
@@ -337,7 +316,7 @@ class GridWorld:
         )
 
         # Finally, add some gridlines
-        for x in range(self.size + 1):
+        for x in range(self.graph.height + 1):
             pygame.draw.line(
                 canvas,
                 0,
@@ -345,13 +324,14 @@ class GridWorld:
                 (self.window_size, pix_square_size * x),
                 width=3,
             )
-            pygame.draw.line(
-                canvas,
-                0,
-                (pix_square_size * x, 0),
-                (pix_square_size * x, self.window_size),
-                width=3,
-            )
+            for x in range(self.graph.width + 1):
+                pygame.draw.line(
+                    canvas,
+                    0,
+                    (pix_square_size * x, 0),
+                    (pix_square_size * x, self.window_size),
+                    width=3,
+                )
 
         if self.render_mode == "human":
             # The following line copies our drawings from `canvas` to the visible window
